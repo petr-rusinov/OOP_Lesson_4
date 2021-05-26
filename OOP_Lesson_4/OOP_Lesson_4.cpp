001ccf12d0ec3af951e30d4780a82355bd8b711c
petr-rusinov <petr-rusinov@yandex.ru>
2021-05-26 17:44:30 +0300

    Added class Hand

--- OOP_Lesson_4/OOP_Lesson_4.cpp
+++ OOP_Lesson_4/OOP_Lesson_4.cpp
@@ -1,11 +1,16 @@
 ﻿#include <iostream>
+#include "ArrayInt.h"
+#include <vector>
 
 //1.Добавить в контейнерный класс, который был написан в этом уроке, методы:
 //	- для удаления последнего элемента массива(аналог функции pop_back() в векторах)
 //	- для удаления первого элемента массива(аналог pop_front() в векторах)
 //	- для сортировки массива
 //	- для вывода на экран элементов.
-//
+
+
+
+
 //2.Дан вектор чисел, требуется выяснить, сколько среди них различных.Постараться использовать максимально быстрый алгоритм.
 //
 //3.Реализовать класс Hand, который представляет собой коллекцию карт.В классе будет одно поле : 
@@ -17,7 +22,52 @@
 
 using namespace std;
 
+enum class eSuit { CLUBS, SPADES, DIAMONDS, HEARTS }; //крести, пики, буби, черви
+enum eValue { ACE = 1, KING, QUEEN, JACK, TEN, NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO, ONE };
+
+const int cardsOneSuit = 14; //количество карт одной масти
+
+class Card
+{
+	eSuit m_suit;
+	eValue m_value;
+	bool m_position;
+public:
+	Card(eSuit suit, eValue value, bool pos) : m_suit(suit), m_value(value), m_position(pos) {}
+	void flip() { m_position = !m_position; }
+	eValue getValue() { return m_value; }
+};
+
+class Hand
+{
+private:
+	vector<Card> m_deck; //колода карт
+public:
+	Hand()
+	{
+		for (int i = 1; i <= cardsOneSuit; i++)
+		{
+			Card card(eSuit::CLUBS, static_cast<eValue>(i), true);
+			m_deck.push_back(card);
+		}
+	}
+	void add(Card& card) { m_deck.push_back(card); }
+	void clear() { m_deck.clear(); }
+	int getValue() 
+	{
+		int s = 0;
+
+		for (Card c : m_deck)
+		{
+			s += static_cast<int>(c.getValue());
+		}
+		return s;
+	}
+};
+
+
 int main()
 {
-	
+	Hand hand;
+	int v = hand.getValue();
 }
